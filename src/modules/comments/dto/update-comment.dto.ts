ff72d475309ffe6068d8c59e6464951f25123949
sarveswarand <sarveswaran.d@tringapps.net>
2026-04-27 17:17:41 +0530

    sanitizehtml/correction

--- src/modules/comments/dto/update-comment.dto.ts
+++ src/modules/comments/dto/update-comment.dto.ts
@@ -1,6 +1,6 @@
 import { IsNotEmpty, IsString } from "class-validator";
 import { Transform } from "class-transformer";
-import * as sanitizeHtml from 'sanitize-html';
+import sanitizeHtml from 'sanitize-html';
 
 export class updateCommentDto {
     @IsString()
